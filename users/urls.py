4a6e8058bcffc6957100edec98a32b065fef766b
JamesMahato <84493570+JamesMahato@users.noreply.github.com>
2022-08-08 13:02:39 +0545

    Delete users directory

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from users import views
-
-urlpatterns = [
-    path("login",views.login_page),
-    path("register",views.register_page),
-    path("logout",views.logout_fun),
-]
